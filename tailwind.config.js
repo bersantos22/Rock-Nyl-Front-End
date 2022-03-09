59db7ca653326d18c67eab9056e15a7270ee79d9
Bruno Filippini <brunofilippini94@gmail.com>
2022-03-09 19:32:48 -0300

    artist list organizada em colunas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        12: "repeat(12, minmax(0, 1fr))",
+      },
+    },
   },
-  plugins: [
-    
-  ],
+  plugins: [],
 };
