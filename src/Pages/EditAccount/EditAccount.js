96b3db18f724e84ff8dd4d0b8d843c057870c4b9
Bruno Filippini <brunofilippini94@gmail.com>
2022-03-09 11:23:39 -0300

    forgot password pages feitas e funcionando !

--- src/Pages/EditAccount/EditAccount.js
+++ src/Pages/EditAccount/EditAccount.js
@@ -1,19 +1,16 @@
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import {AuthContext} from "../../contexts/authContext"
-import {useEffect, useState, useContext} from 'react'
-import { api } from '../../api/api';
-import { Button } from '../../Components/Button/index'
-import { FormField } from '../../Components/Forms/FormField';
+import { AuthContext } from "../../contexts/authContext";
+import { useEffect, useState, useContext } from "react";
+import { api } from "../../api/api";
+import { Button } from "../../Components/Button/index";
+import { FormField } from "../../Components/Forms/FormField";
 
-export function EditAccount(){
-
+export function EditAccount() {
   const { loggedInUser } = useContext(AuthContext);
   const { id } = useParams();
 
-
   const [form, setForm] = useState({
-      name:''
+    name: "",
   });
 
   useEffect(() => {
@@ -33,49 +30,47 @@ export function EditAccount(){
     setForm({ ...form, [e.target.name]: e.target.value });
   }
 
-
   function handleSubmit(e) {
     e.preventDefault();
-    
-    for(let key in form){
-        if(!form[key]){
-            window.alert(`Preencher o campo ${key}.`);
-            return;
-        }
+
+    for (let key in form) {
+      if (!form[key]) {
+        window.alert(`Preencher o campo ${key}.`);
+        return;
+      }
     }
 
     async function updateForm(id) {
       try {
-
         await api.patch(`account/profile/update`, {
           ...form,
           userId: loggedInUser.token._id,
         });
-        window.location.href="/myAccount";
-       
+        window.location.href = "/myAccount";
       } catch (err) {
         console.log(err);
-       
       }
     }
     updateForm(id);
   }
 
-    return(
-        <div className="h-screen mt-5 mb-5 flex flex-col items-center">
-            <div className="title2 mt-1 mb-5">
-            <p>Edit Account</p>
-            </div>
-                <form className='bg-white shadow-2xl rounded px-12 pt-6 pb-8 mb-5 w-1/4' onSubmit={handleSubmit}>
-
-                <FormField
-                label='Full Name'
-                id='name'
-                name='name'
-                value={form.name}
-                onChange={handleChange} 
-                />
-            {/*  <FormField
+  return (
+    <div className="h-screen mt-5 mb-5 flex flex-col items-center">
+      <div className="title2 mt-1 mb-5">
+        <p>Edit Account</p>
+      </div>
+      <form
+        className="bg-white shadow-2xl rounded px-12 pt-6 pb-8 mb-5 w-1/4"
+        onSubmit={handleSubmit}
+      >
+        <FormField
+          label="Full Name"
+          id="name"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+        />
+        {/*  <FormField
                 label='E-mail'
                 type='email'
                 id='emailEdit'
@@ -84,11 +79,13 @@ export function EditAccount(){
                 onChange={handleChange} 
                 /> */}
 
-
-                <Button type="submit" className="bg-green-600 hover:bg-green-400 text-white font-bold py-2 px-4 mt-3 rounded focus:outline-none focus:shadow-outline" >
-                Submit Edit
-                </Button>
-                </form>
-        </div>
-    )
-    }
+        <Button
+          type="submit"
+          className="bg-green-600 hover:bg-green-400 text-white font-bold py-2 px-4 mt-3 rounded focus:outline-none focus:shadow-outline"
+        >
+          Submit Edit
+        </Button>
+      </form>
+    </div>
+  );
+}
---
+++ src/Pages/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { api } from "../../api/api";
+
+import { FormField } from "../../Components/Forms/FormField";
+import { ErrorAlert } from "../../Components/ErrorAlert";
+import { Button } from "../../Components/Button";
+
+export function ForgotPassword() {
+  const [form, setForm] = useState({
+    email: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(null);
+
+      await api.post("/password/forgot-password", form);
+      setLoading(false);
+      window.alert("Senha enviada para seu e-mail");
+    } catch (error) {
+      setLoading(false);
+      if (error.response) {
+        console.log(error.response);
+        setError(error.response.data._message);
+      } else {
+        setError("Algo deu errado");
+      }
+    }
+  }
+
+  return (
+    <div className="h-screen mt-5 mb-5 flex flex-col items-center">
+      <div className="title2 mt-1 mb-5">
+        <p>Forgot Password</p>
+      </div>
+
+      <form
+        className="bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-4 mb-5 w-1/2"
+        onSubmit={handleSubmit}
+      >
+        <FormField
+          type="email:"
+          label="E-mail"
+          id="signUpEmail"
+          name="email"
+          onChange={handleChange}
+          value={form.email}
+          required={true}
+          readOnly={loading}
+        />
+
+        <Button
+          type="submit"
+          disabled={loading}
+          className="bg-stone-800 hover:bg-amber-500 text-white font-bold py-2 px-4 mt-3 rounded focus:outline-none focus:shadow-outline"
+        >
+          {loading ? (
+            <div className="animate-spin" role="status">
+              <span className="hidden">Loading...</span>
+            </div>
+          ) : (
+            "Reset Password"
+          )}
+        </Button>
+
+        {error ? <ErrorAlert>{error}</ErrorAlert> : null}
+      </form>
+    </div>
+  );
+}
