b6b633169ff64d56743d90af7fe6f41373162390
Bruno Filippini <brunofilippini94@gmail.com>
2022-03-09 17:55:12 -0300

    update search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ export function SearchBar(props) {
   return (
     <div>
       <input
-        className="placeholder:italic block bg-white w-1/4 border border-slate-300 rounded-md py-1 pl-4 pr-3 shadow-l focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-base"
+        className="placeholder:italic block bg-white w-1/4 border border-slate-300 rounded-md mb-4 py-1 pl-4 pr-3 shadow-l focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-base"
         type="text"
         area="searchParams"
         placeholder={props.placeholder}
