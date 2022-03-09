8f5b602f269d7c69848dbb65856f0d87d2d30c27
Bruno Filippini <brunofilippini94@gmail.com>
2022-03-09 11:33:03 -0300

    button create produtor in Myprofile only role ADM

--- src/Pages/MyAccount/MyAccount.js
+++ src/Pages/MyAccount/MyAccount.js
@@ -3,61 +3,89 @@ import { useState, useEffect } from "react";
 import { api } from "../../api/api";
 import { Button } from "../../Components/Button";
 import { Link } from "react-router-dom";
-import './MyAccount.css'
+import "./MyAccount.css";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/authContext";
 
 export function MyAccount() {
-    const [test, setTest] = useState({
-      _id: "",
-      name: "",
-      email: ""
-    
-    });
-  
-    useEffect(() => {
-      async function fechUser() {
-        try {
-          const response = await api.get("account/profile");
-          console.log(response.data);
-          setTest({ ...response.data });
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      fechUser();
-    }, []);
+  const { loggedInUser } = useContext(AuthContext);
+  const [test, setTest] = useState({
+    _id: "",
+    name: "",
+    email: "",
+  });
 
-    async function handleDelete() {
+  useEffect(() => {
+    async function fechUser() {
       try {
-        await api.delete('/account/delete-account');
-        window.location.href='/login'
+        const response = await api.get("account/profile");
+        console.log(response.data);
+        setTest({ ...response.data });
       } catch (error) {
         console.error(error);
       }
     }
-  
-    return (
-      <div className="h-screen mt-5 mb-5 flex flex-col">
-        <div className="title2 mt-5 mb-5">
-          <p>MY PROFILE</p>
+    fechUser();
+  }, []);
+
+  async function handleDelete() {
+    try {
+      await api.delete("/account/delete-account");
+      window.location.href = "/login";
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="h-screen mt-5 mb-5 flex flex-col">
+      <div className="title2 mt-5 mb-5">
+        <p>MY PROFILE</p>
+      </div>
+      <div className="bg-white shadow-2xl rounded ml-12 px-11 pt-6 pb-8 w-2/5">
+        <div className="subtitle mx-4 mb-2">
+          <p>
+            <b>Id: </b>
+            {`${test._id}`}
+          </p>
+          <p>
+            <b>Name: </b>
+            {`${test.name}`}
+          </p>
+          <p>
+            <b>E-mail: </b>
+            {`${test.email}`}
+          </p>
         </div>
-        <div className="bg-white shadow-2xl rounded ml-12 px-11 pt-6 pb-8 w-2/5">
-            <div className="subtitle mx-4 mb-2">
-                <p><b>Id: </b>{ `${test._id}`}</p>
-                <p><b>Name: </b>{`${test.name}`}</p>
-                <p><b>E-mail: </b>{ `${test.email}`}</p>
-            </div>
-            <div className="ml-4">
-                <Link to={`/editAccount`} >
-                    <Button className='bg-green-800 hover:bg-green-400 text-white font-bold py-2 px-4 mt-3 rounded focus:outline-none focus:shadow-outline mr-6'>Edit Perfil</Button>
-                </Link>
+        <div className="ml-4">
+          <Link to={`/editAccount`}>
+            <Button className="bg-green-800 hover:bg-green-400 text-white font-bold py-2 px-4 mt-3 rounded focus:outline-none focus:shadow-outline mr-6">
+              Edit Perfil
+            </Button>
+          </Link>
 
-                <Link to={`/___ROTA_DO_CARRINHO_AQUI___/${test._id}}`} >
-                    <Button className='bg-amber-600 hover:bg-amber-400 text-white font-bold py-2 px-6 mt-3 rounded focus:outline-none focus:shadow-outline mr-6 '>My Bag </Button>
-                </Link>
+          <Link to={`/___ROTA_DO_CARRINHO_AQUI___/${test._id}}`}>
+            <Button className="bg-amber-600 hover:bg-amber-400 text-white font-bold py-2 px-6 mt-3 rounded focus:outline-none focus:shadow-outline mr-6 ">
+              My Bag{" "}
+            </Button>
+          </Link>
 
-                <Button className='bg-red-800 hover:bg-red-400 text-white font-bold py-2 px-6 mt-3 rounded focus:outline-none focus:shadow-outline mr-6 ' onClick={handleDelete}>Delete </Button>
-            </div>
+          <Button
+            className="bg-red-800 hover:bg-red-400 text-white font-bold py-2 px-6 mt-3 rounded focus:outline-none focus:shadow-outline mr-6 "
+            onClick={handleDelete}
+          >
+            Delete{" "}
+          </Button>
+
+          {loggedInUser.user.role === "ADMIN" ? (
+            <Link to={"/adm/create-product"}>
+              <Button className="bg-blue-600 hover:bg-amber-400 text-white font-bold py-2 px-6 mt-3 rounded focus:outline-none focus:shadow-outline mr-6 ">
+                Create Product
+              </Button>
+            </Link>
+          ) : null}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
